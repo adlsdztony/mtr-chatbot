b979210513551894a7bf0dd6a09ca59615f0307e
adlsdztony <zzl0712@connect.hku.hk>
2025-10-09 13:18:33 +0800

    feat: update chat model to qwen3:30b in docker-compose and settings

--- utils/settings.py
+++ utils/settings.py
@@ -3,8 +3,8 @@ import os
 PORT = 11434
 CHAT_API_URL = os.getenv("OLLAMA_BASE_URL", f"http://localhost:{PORT}")
 
-CHAT_MODEL = os.getenv("OLLAMA_CHAT_MODEL", "deepseek-r1:8b")  
-# qwen3:32b
+CHAT_MODEL = os.getenv("OLLAMA_CHAT_MODEL", "qwen3:30b")  
+# qwen3:30b
 
 VISION_MODEL = os.getenv("OLLAMA_VISION_MODEL", "qwen2.5vl:7b")  
 # gemma3:27b
